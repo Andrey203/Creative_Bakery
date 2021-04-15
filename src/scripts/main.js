c9221fde4b1b48fb40815b8307b88381b69c0394
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-15 18:04:50 +0300

    first two blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const $ = window.$;
+
+$('.burger-menu-wrapper').click(() => {
+  const navTopState = $('.header__sidebar').css('top');
+
+  if (navTopState !== '0px') {
+    $('.header__sidebar')
+      .css('top', '0');
+
+    $('.burger-menu-line')
+      .css('transform', 'translate(-30px)');
+
+    $('.burger-menu')
+      .css('transform', 'translate(30px)');
+
+    $('.burger-menu-close')
+      .css('transform', 'rotate(45deg) translate(0px)');
+
+    $('.burger-menu-close-line')
+      .css('transform', 'rotate(-45deg) translate(0px)');
+  } else {
+    $('.header__sidebar')
+      .css('top', '-100%');
+
+    $('.burger-menu-close-line')
+      .css('transform', 'rotate(-45deg) translate(30px)');
+
+    $('.burger-menu-close')
+      .css('transform', 'rotate(45deg) translate(30px)');
+
+    $('.burger-menu')
+      .css('transform', 'translate(0px)');
+
+    $('.burger-menu-line')
+      .css('transform', 'translate(0px)');
+  }
+});
